f9904bde80e5851a53187e2f640821bc8f342d8d
OscV12 <oscventura12@gmail.com>
2025-03-22 12:53:44 -0400

    Lab 4 exercise

---
+++ Module1/LabExercises/exercise4.js
@@ -0,0 +1,33 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    console.log("Error: Division by zero is not allowed.");
+    return null;
+  }
+  return a / b;
+}
+
+let num1 = 1298;
+let num2 = 587;
+
+console.log("Addition: " + add(num1, num2));
+console.log("Subtraction: " + subtract(num1, num2));
+console.log("Multiplication: " + multiply(num1, num2));
+console.log("Division: " + divide(num1, num2));
+
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+greet("Ferris!");
